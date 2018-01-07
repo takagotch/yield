b73700703e5407ab8160a1ad6dada99e066d8549
takagotch <dyaccb@gmail.com>
2018-01-07 17:22:33 +0900

    @array.each {|x| yeild x}

--- array.rb
+++ array.rb
@@ -7,6 +7,23 @@ a[1]                    #=> [3, 4, 5] #second_group, 1
 
 a[1][2]                 #=> [5]       #second_group0 && second_group+third_number
 
+#<<
+#1
+coins = []
+coins << 1              #=> [1]
+coins << 5 << 10        #=> [1, 5, 10]
+#2
+class A
+  class << self
+    def load(fname)     #self.load(fname)
+    end
+    def write(fname)    #*multi .method* <<
+    end
+    #...
+  end
+end
+#3 has_many
+@price << @balance
 
 #ex2
 
