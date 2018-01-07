eef9e783eaeb09430e4bd2c41ecf22a212ecadda
takagotch <dyaccb@gmail.com>
2018-01-07 16:44:33 +0900

    ARGV[0] 0: first_number

---
+++ array.rb
@@ -0,0 +1,19 @@
+# array 0: 1, 1: 2, 2: 3, ...
+# *0*: first_number
+a = [[1, 2], [3, 4, 5]] 
+
+a[0]                    #=> [1, 2]    #first_group, 0
+a[1]                    #=> [3, 4, 5] #second_group, 1
+
+a[1][2]                 #=> [5]       #second_group0 && second_group+third_number
+
+
+#ex2
+
+[[101, 111, 112, 333],
+ [103, 120, 100, 144],
+ [115, 123, 165, 197]]
+
+method_method2[0][0]   #=> 101
+
+
